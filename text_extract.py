da59175fb96904bc8edca735de40dadeea039745
Keigh Rim <keigh.rim@gmail.com>
2023-10-31 16:39:55 -0400

    updated regex to clean up gold files

--- text_extract.py
+++ text_extract.py
@@ -9,7 +9,8 @@ suffix = '-transcript'
 
 def extract_text(gbh_json_dir):
     for j in pathlib.Path(gbh_json_dir).glob('*.json'):
-        guid = j.name.split('.')[0][:len(suffix) * -1]
+        guid = re.search(r'(cpb-aacip[-_][a-z0-9-]+).', j.name).group(1)
+        guid = guid.replace('-transcript', '')
         if guid in dirs.exclude_guid:
             continue
         with open(j, 'r') as f:
@@ -26,9 +27,9 @@ def extract_text(gbh_json_dir):
         dirs.gold_texts_dir.mkdir(parents=True, exist_ok=True)
         with (open(dirs.gold_texts_dir / f'{guid}{suffix}.txt', 'w', encoding='utf-8') as txt):
             # exising regex
-            text = re.sub("[\<\[].*?[\>\]]", "", text)
+            text = re.sub("[\<\[].*?[\>\]](: )?", "", text)
             # new regex to remove parens
-            text = re.sub("\((.*?)\)","",text)
+            text = re.sub("\([^ )]+( [^ )]+)?\)","",text)
             # new regex to remove '^[:. ]+'
             text = re.sub("^[:. ]+", "", text)
 
