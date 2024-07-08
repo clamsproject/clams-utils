8e3e1a1021d11f4768de0f30e88a8020f7bfedb1
selenasong <jialinsong@brandeis.edu>
2024-07-08 10:41:21 -0400

    modularize some functions, create main

--- clams_utils/aapb/newshour_transcript_cleanup.py
+++ clams_utils/aapb/newshour_transcript_cleanup.py
@@ -1,6 +1,4 @@
 import os
-import argparse
-
 import json
 import re
 
@@ -21,31 +19,65 @@ def extract_text_from_plain_text(transcript_text):
     """
     Given a plain text read from a txt file, extract texts by removing speakers' names
     """
-    # remove titles that are 'FOCUS'
+    titles_removed = clean_titles(transcript_text)
+    brackets_removed = clean_brackets(titles_removed)
+    speakers_removed = clean_speakers(brackets_removed)
+    return speakers_removed
+
+def clean_titles(transcript_text):
+    """
+    Given a plain text read from a txt file, clean the titles in the transcript,
+    including "Focus", "Intro", and "News Summary"
+    """
     focus = r'^FOCUS-.*\n'
     focus_removed = re.sub(focus, '\n', transcript_text)
-    # remove titles that are 'Intro', usually showing up at the beginning of a transcript
+
     intro = r'\nI(?i:ntro)[\n\s]'
     intro_removed = re.sub(intro, '\n', focus_removed)
-    # remove titles that are 'News summary', which can show up in either upper or lower cases
+
+    newsmaker = r'\nN(?:ewsmaker)\n'
+    newsmaker_removed = re.sub(newsmaker, '\n', intro_removed)
+
     news_summary = r'(?<=\s|\n)N(?i:ews)\s(?i:summary)(?=\n)'
-    news_summary_removed = re.sub(news_summary, '\n', intro_removed)
-    # remove parenthesis and square brackets, which are usually unspoken words
+    all_titles_removed = re.sub(news_summary, '\n', newsmaker_removed)
+
+    return all_titles_removed
+
+def clean_brackets(transcript_text):
+    """
+    Given a plain text read from a txt file, clean the text with brackets,
+    including square brackets and parentheses
+    """
     brackets = r'\s[\[\(].*[\]\)]'
-    square_bracket_removed = re.sub(brackets, " ", news_summary_removed)
-    # remove the position of the speaker, usually appears after their names and before the column (e.g., "WALTER MONDALE, Democratic presidential candidate:")
-    speaker_position = r'[A-Z],\s[a-zA-Z\.\'-,\s]*(?=:)'
-    speaker_position_removed = re.sub(speaker_position, " ", square_bracket_removed)
-    # remove the speaker's title (e.g., Mr., Prof., Sen.)
-    newline_speaker_title = r'(?<=\n)(Rep\.|Dr\.|Sen\.|Mr\.|Ms\.|Mrs\.|Prof\.|Pres\.)(?=\s)'
-    newline_speaker_title_removed = re.sub(newline_speaker_title, '', speaker_position_removed)
+    brackets_removed = re.sub(brackets, "", transcript_text)
+
+    return brackets_removed
+
+def clean_speakers(transcript_text):
+    """
+    Given a plain text read from a txt file, clean speakers' names in the transcript.
+    This can be their last name, full name, names with titles (e.g., Prof., Mr., Sen.),
+    and names with a short intro on who they are (e.g., WALTER MONDALE, Democratic presidential candidate)
+    """
+    speaker_intro = r'(?<=[A-Z]),\s[a-zA-Z\.\'-,\s]*(?=:)'
+    text = re.sub(speaker_intro, "", transcript_text)
+
+    speaker_titles = r'(?<=\n)(?i:Rep\.|Dr\.|Sen\.|Mr\.|Ms\.|Mrs\.|Prof\.|Pres\.)(?=\s)'
+    text = re.sub(speaker_titles, "", text)
+
     # remove the speaker's name using newline
-    newline_speaker = r'(?=\n)[A-Z][a-zA-Z\s\'\.-]*[A-Z\s]:(?!\.)'
-    newline_speaker_removed = re.sub(newline_speaker, " ", newline_speaker_title_removed)
-    # remove unprocessed names
-    speaker_name = r'(?=[\.\?\-\s])\s[A-Z][a-zA-Z\s\'-]*[A-Z\s]:(?!\.)'
-    speaker_name_removed = re.sub(speaker_name, " ", newline_speaker_removed)
-    return speaker_name_removed
+    newline_speaker = r'\n[A-Z][a-zA-Z\s\'\.\-]*[A-Z]:'
+    text = re.sub(newline_speaker, " ", text)
+
+    # remove the speaker's name inline
+    speaker_name_inline = r'(?=[\.\?\-\s])\s[A-Z][a-zA-Z\s\'-]*[A-Z]:(?!\.)'
+    text = re.sub(speaker_name_inline, "\n", text)
+
+    # remove the speaker's title inline
+    speaker_title_inline = r'(?i:Rep\.|Dr\.|Sen\.|Mr\.|Ms\.|Mrs\.|Prof\.|Pres\.|Gen\.)(?=\n)'
+    text = re.sub(speaker_title_inline, "", text)
+
+    return text
 
 def is_json(in_file):
     """
@@ -78,19 +110,15 @@ def extract_text(in_file, out_file):
         with open(out_file, 'w') as f:
             f.write(text)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--transcriptPath", action='store', help="path to the directory of all text files")
-    parser.add_argument("--cleanTranscriptPath", action='store', help="path to the directory of cleaned transcripts")
-    parsed_args = parser.parse_args()
-    if not os.path.exists(parsed_args.cleanTranscriptPath):
-        os.makedirs(parsed_args.cleanTranscriptPath)
-    for file in os.listdir(parsed_args.transcriptPath):
-        in_file = os.path.join(parsed_args.transcriptPath, file)
+def clean(transcript_path, cleaned_transcript_path):
+    if not os.path.exists(cleaned_transcript_path):
+        os.makedirs(cleaned_transcript_path)
+    for file in os.listdir(transcript_path):
+        in_file = os.path.join(transcript_path, file)
         if file.endswith(".txt"):
-            out_file = os.path.join(parsed_args.cleanTranscriptPath, file)
+            out_file = os.path.join(cleaned_transcript_path, file)
         elif file.endswith(".json"):
-            out_file = os.path.join(parsed_args.cleanTranscriptPath, file.replace(".json", ".txt"))
+            out_file = os.path.join(cleaned_transcript_path, file.replace(".json", ".txt"))
         else:
             continue
         extract_text(in_file, out_file)
