ae50f28edbb94e54c986ffc438ef9391136020a5
Jordan Youner <jordanyouner@brandeis.edu>
2024-08-13 16:57:10 -0400

    Fixed issue with generating post request

--- clams_utils/bllc/predretriever.py
+++ clams_utils/bllc/predretriever.py
@@ -4,7 +4,7 @@ import requests
 import hashlib
 
 
-def download_preds(storage_url, pipeline_json: json, folder_name: os.path = None):
+def download_preds(storage_url, pipeline_json, folder_name: os.path = None):
     import tempfile
     # code adapted from Keigh's goldretriever, which is adapted from Angela Lam
     if folder_name is None:
@@ -14,9 +14,10 @@ def download_preds(storage_url, pipeline_json: json, folder_name: os.path = None
     if not (len(os.listdir(folder_name)) == 0):
         raise Exception("The folder '" + folder_name + "' already exists and is not empty")
     # send a POST request to the storage api
-    response = requests.post(storage_url, pipeline_json, headers={"Content-Type": "application/json"})
+    # note it expects a string so we use json.dumps
+    response = requests.post(storage_url, json.dumps(pipeline_json), headers={"Content-Type": "application/json"})
     # read the response as json. keys are guids and values are serialized mmif files
-    mmifs = json.loads(response.text)
+    mmifs = json.loads(response.content)
     # iterate through json and save each mmif as its own file
     for mmif in mmifs:
         filename = str(mmif) + ".mmif"
